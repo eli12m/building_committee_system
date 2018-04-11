38a64d50846473f4e0c5de6f543c2c9ff3ecf626
eli12m <elimasri@gmail.com>
2018-04-11 03:25:39 +0300

    landing and login pages view

--- app/app.js
+++ app/app.js
@@ -2,8 +2,9 @@ var app = angular.module( "homeSystemApp", ["ngRoute"] );
 
 app.config(function($routeProvider) {
     $routeProvider
-    .when("/", { templateUrl : "main.html" })
+    .when("/", { templateUrl : "landingPage.html" })
     .when("/messages", { templateUrl : "/app/messages/messages.html", controller : "messagesCtrl" })
     .when("/tenants", { templateUrl : "/app/Tenants/tenants.html", controller : "tenantsCtrl" })
+    .when("/login", { templateUrl : "/app/login/login.html", controller : "loginCtrl" })
     .otherwise({redirectTo : "/"});
 });
