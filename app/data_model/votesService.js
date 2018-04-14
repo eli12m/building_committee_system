2446a921570346d0e7cc0ab394dc14d1d8d8a8f0
eli12m <elimasri@gmail.com>
2018-04-14 14:10:23 +0300

    loading votes of specific voting

---
+++ app/data_model/votesService.js
@@ -0,0 +1,32 @@
+app.factory( "votesService", function(){
+
+    function Vote( voteByStr, voteValStr )
+    {
+        this.voteBy     = voteByStr;
+        this.voteVal    = voteValStr;
+        this.getVotedBy = function(){ return this.voteBy; }
+        this.setVotedBy = function( voteByStr ){ this.voteBy = voteByStr; }
+        this.getVoteVal = function(){ return this.voteVal; }
+        this.setVoteVal = function( voteValStr ){ this.voteVal = voteValStr; }
+    }
+
+    function cloneVotesArrFunc( votes )
+    {
+        var i       = 0;
+        var vote    = null;
+        var cpVotes = [];
+
+        for( i = 0; i < votes.length; i++ )
+        {
+            vote = new Vote( votes[i].voteBy, votes[i].voteVal );
+
+            cpVotes.push( vote );
+        }
+
+        return cpVotes;
+    }
+
+    return{
+        cloneVotesArrMethod: cloneVotesArrFunc
+    };
+});
--- app/data_model/votingsService.js
+++ app/data_model/votingsService.js
@@ -1,14 +1,15 @@
-app.factory( "votingsService", function( $http, $q ){
+app.factory( "votingsService", function( $http, $q, votesService ){
     var votings          = [];
     var counter          = 3; /*todo: to change this that it will be read from json and not hard coded*/
     var loadVotingsFlag  = false;
     
-    function Voting( id, title, details, endDate )
+    function Voting( id, title, details, endDate, votes )
     {
         this.id                = id;
         this.title             = title;
         this.details           = details;
         this.endDate           = new Date( endDate );
+        this.votes             = votes;
         this.getId             = function(){ return this.id; }
         this.getTitle          = function(){ return this.title; }
         this.setTitle          = function( title ){ this.title = title; }
@@ -16,6 +17,32 @@ app.factory( "votingsService", function( $http, $q ){
         this.setDetails        = function( details ){ this.details = details; }
         this.getEndDate        = function(){ return this.endDate; }
         this.setEndDate        = function( endDate ){ this.endDate = endDate; }
+        this.getVotes          = function(){ return this.votes; }
+        this.setVotes          = function( votes ){ this.votes = votes; }
+        this.addVoteToVoting   = function( vote )
+        {
+            this.votes.push( vote );
+        }
+        this.copyVotesArr = function( votes )
+        {
+            this.votes = votesService.cloneVotesArrMethod( votes );  
+        }
+        this.isTenantVotedOnVoting = function( voterStr )
+        {
+            var i   = 0;
+            var res = false;
+
+            for( i = 0; i < this.votes.length; i++ )
+            {
+                if( voterStr === this.votes[i].getVotedBy() )
+                {
+                    res = true;
+                    break;
+                }
+            }
+
+            return res;
+        }
     }
 
     function loadVotingsFunc()
@@ -34,13 +61,13 @@ app.factory( "votingsService", function( $http, $q ){
                 //on success
                 var i = 0;
 
-                //We cannot do here messages = [] because the outside already hold actors and if we do that we change the pointer. For example two collectors that use this service. The first collector call to this function and get an array of actors. Then the second collector call to this function and do messages = [] and by this give a new pointer to messages so the first collector still looked on the old pointer of messages.
-                //so to empty the array instead of doing messages = [] do actors.splice( 0, messages.length )
+                //We cannot do here votings = [] because the outside already hold actors and if we do that we change the pointer. For example two collectors that use this service. The first collector call to this function and get an array of actors. Then the second collector call to this function and do messages = [] and by this give a new pointer to messages so the first collector still looked on the old pointer of messages.
+                //so to empty the array instead of doing votings = [] do actors.splice( 0, messages.length )
 
                 votings.splice( 0, votings.length );
 
                 for( i = 0; i < response.data.length; i++ ){
-                    votings.push( new Voting( parseInt( response.data[i].votingId ), response.data[i].title, response.data[i].details, response.data[i].endDate ) );
+                    votings.push( new Voting( parseInt( response.data[i].votingId ), response.data[i].title, response.data[i].details, response.data[i].endDate, votesService.cloneVotesArrMethod( response.data[i].votes ) ) );
                 }
         
                 asyncLoad.resolve( votings );
@@ -48,7 +75,7 @@ app.factory( "votingsService", function( $http, $q ){
                 //on failure
                 loadVotingsFlag = false;
 
-                alert( "Error:" + response );
+                alert( "Error:" + response );/*todo: to change that*/
                 asyncLoad.reject( votings ); 
             }); 
         }
