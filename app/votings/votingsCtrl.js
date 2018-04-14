5543d49b460ecb5aede682b47eede0f064662458
eli12m <elimasri@gmail.com>
2018-04-14 15:04:38 +0300

    disble vote buttons for active tenant that vote on that voting

--- app/votings/votingsCtrl.js
+++ app/votings/votingsCtrl.js
@@ -2,6 +2,14 @@ app.controller( "votingsCtrl", function( $scope, loginService, votingsService ){
     $scope.isShowBtn = function(){
         return loginService.isLoginTenantCommitteeMemberMethod();
     }
+    $scope.isDisableBtn = function( voting ){
+        var emailActiveTenant = loginService.getActiveTenantMethod().getEmail();
+        var res               = false;
+
+        res = voting.isTenantVotedOnVoting( emailActiveTenant );
+
+        return res;
+    }
 
     votingsService.loadVotingsMethod().then( function( votings ){
         $scope.votings = votings;
