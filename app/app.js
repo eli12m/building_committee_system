fbd3fc320caaedddcf0ee9bc3e2d224af4c2932a
eli12m <elimasri@gmail.com>
2018-04-23 14:28:07 +0300

    routing tenants dashboard

--- app/app.js
+++ app/app.js
@@ -7,5 +7,6 @@ app.config(function($routeProvider) {
     .when("/tenants", { templateUrl : "/app/Tenants/tenants.html", controller : "tenantsCtrl" })
     .when("/login", { templateUrl : "/app/login/login.html", controller : "loginCtrl" })
     .when("/votings", { templateUrl : "/app/votings/votings.html", controller : "votingsCtrl" })
+    .when("/tenants_dashboard", { templateUrl : "/app/tenants_dashboard/tenantsDashboard.html" })
     .otherwise({redirectTo : "/"});
 });
