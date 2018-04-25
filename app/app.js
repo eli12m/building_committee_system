6800f8cdf2d19c9d2bad118897f96ed15c76fcd6
eli12m <elimasri@gmail.com>
2018-04-25 17:33:37 +0300

    more clean

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ app.config(function($routeProvider) {
     $routeProvider
     .when("/", { templateUrl : "landingPage.html" })
     .when("/messages", { templateUrl : "app/messages/messages.html", controller : "messagesCtrl" })
-    .when("/tenants", { templateUrl : "app/Tenants/tenants.html", controller : "tenantsCtrl" })
+    .when("/tenants", { templateUrl : "app/tenants/tenants.html", controller : "tenantsCtrl" })
     .when("/login", { templateUrl : "app/login/login.html", controller : "loginCtrl" })
     .when("/votings", { templateUrl : "app/votings/votings.html", controller : "votingsCtrl" })
     .when("/tenants_dashboard", { templateUrl : "app/tenants_dashboard/tenantsDashboard.html" })
